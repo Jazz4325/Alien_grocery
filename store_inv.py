1ebfa77a66ed5e97b16a871d477de7e1003014ce
Jazz4325 <vincent.budak@gmail.com>
2025-08-20 17:55:23 -0700

    working on store_inv

--- store_inv.py
+++ store_inv.py
@@ -1,29 +1,48 @@
 from item_list import *
+from typing import Optional
+
+StockType = dict[type, int]
 
 class Department():
 
-    def __init__(self, name, stock={}):
+    def __init__(self, name, par: StockType={}):
         self.name = name
-        self.stock = stock
+        self.par = par
+        self.stock: StockType = {}
+        self.restock_dept()
 
-    # we are not initing all items here, we are only making a blueprint of what can/cannot be in a specific department when IT is inited
+    # restock the whole dept to pars
+    def restock_dept(self) -> None:
+        for item_class_name in self.par:
+            self.restock_item(item_class_name)
+
+    # restock individual items 
+    def restock_item(self, item_class_name) -> None:
+        par_quantity = self.par[item_class_name]
+        self.stock[item_class_name] = par_quantity
+        
+        """ restock specific subclasses
+        for item_class_name in self.par:
+            if isinstance(item_class_name, Edible):
+                restock
+        """
+
+    # can the dept ever carry this item?
+    def carries_item(self, item_class_name) -> bool: # type annotation!
+        return item_class_name in self.stock # returns true if the thing is in stock, else returns false
+
+    # if so, how many are there now?    
+    def get_item_quantity(self, item_class_name) -> int:
+        if not self.carries_item(item_class_name):
+            return 0 
+        return self.stock[item_class_name]
+
+    # are there any of this item in this dept now?
+    def item_in_stock(self, item_class_name) -> bool:
+        return self.get_item_quantity(item_class_name) > 0
     
-    # method to add_stock / restock? / update_stock once department is already inited
-    
-    def check_stock(self, item_class_name):
-        print(f"checking if {item_class_name} is in stock...")
-        if item_class_name in self.stock:
-            item_count = self.stock[item_class_name]
-            # [item_class_name] uses the value of item_class_name as a key to look up the corresponding value in the dictionary.
-            # That value (the number in stock) is retrieved and assigned to item_count.
-            print(f"there are {item_count} in stock.")
-            return item_count
-        else:
-            print(f"{self.name} doesn't carry {item_class_name}... check another department?")
-            return 0
-    
-    def take_item(self, item_class_name):
-        if self.check_stock(item_class_name) > 0:
+    def take_item(self, item_class_name) -> Optional[Item]:
+        if self.item_in_stock(item_class_name):
             item = item_class_name()
             # using item_class_name as a function calls init on that item, spawning it
             print(item)
@@ -31,3 +50,4 @@ class Department():
             return item
         else:
             print(f"there are no {item_class_name}'s in stock...")
+    
